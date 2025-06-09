6b9a83d255d6b38eff10777847a3d090d12981c0
funprogrammer-lab <praneel.alagi@gmail.com>
2025-06-09 19:05:18 -0400

    logging and exception

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from typing import List
 HYPHEN_E_DOT='-e .'
 def get_requirements(file_path:str)->List[str]:
     '''
-    this funciton will returen the list of requirements
+    this function will return the list of requirements
     '''
     requirements=[]
     with open(file_path) as file_obj:
